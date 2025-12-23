4175b35886824e74196aa244b8de804958c6eaa2
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2025-12-23 18:39:07 +0530

    created app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,11 +4,11 @@ const config: CapacitorConfig = {
   appId: "com.lovable.crocussativus",
   appName: "crocus-sativus-spotlight",
   webDir: "dist",
-  bundledWebRuntime: false,
-  server: {
-    url: "https://80577687-db59-45ec-8d74-e5a4a04d77f9.lovableproject.com?forceHideBadge=true",
-    cleartext: true,
-  },
+  // bundledWebRuntime: false,
+  // server: {
+  //   url: "https://80577687-db59-45ec-8d74-e5a4a04d77f9.lovableproject.com?forceHideBadge=true",
+  //   cleartext: true,
+  // },
 };
 
 export default config;
