56bf9fb0fa88f53ea3027abc2eb5aa6ce2eb1222
Ibrahim-Saasa <ibrahimsaasa@gmail.com>
2026-03-12 00:07:37 +0530

    built the app

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/crocus-sativus-spotlight/" : "/",
+export default defineConfig({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean,
-  ),
+  plugins: [react(), componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
